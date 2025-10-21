1c01b6f2e9547b0f197a2c438d945d574eab001e
Fanual Gebreyesus <132348804+mycookie5@users.noreply.github.com>
2025-10-21 11:18:12 +0200

    code cleanup for reuse.

--- backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ec107a2da7e469851f8165108d5705c13d39f3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
