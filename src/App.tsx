fbb4bd3bddb8fe8b62dbf75cb823e3abf649fc72
Fanual Gebreyesus <132348804+mycookie5@users.noreply.github.com>
2025-11-15 15:00:13 +0100

    made changes to database

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,11 @@ import Main from './partials/Main';
 import { useLoaderData } from "react-router-dom";
 import { useState, useEffect } from 'react';
 
-// turn off when not needed for debugging
-const showBootstrapBreakpoints = true;
-
 export default function App() {
 
-  // scroll to top when the route changes
   useLocation();
   window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
 
-  // get logged in user (also see loader in main.tsx)
   const [user, setUser] = useState();
   const userFromLoader = useLoaderData();
 
@@ -21,10 +16,9 @@ export default function App() {
     setUser(userFromLoader);
   }, [userFromLoader]);
 
-  // making the setter global so it can be used from
-  // Login and Logout pages
   (globalThis as any).setUser = setUser;
 
+  console.log('App render, user=', user);
   return !user ? null : <>
     <Header user={user} />
     <Main />
